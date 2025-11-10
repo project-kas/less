d2833e957c51ef03dfe1fd89eda2e1f8c1413b47
project-kas <kasewajoseph@gmail.com>
2025-11-10 15:48:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login(){
+    location.href = "data.html";
+}
+function reg(){
+    location.href = "reg.html";
+}
+function log(){
+    location.href = "index.html";
+}
+function show(){
+    location.href = "index.html";
+    alert("hello");
+}
