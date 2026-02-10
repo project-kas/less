81b3cd586036d04444c8344028524b66d20090ef
project-kas <kasewajoseph@gmail.com>
2026-02-10 12:24:26 +0300

    c

--- data.js
+++ data.js
@@ -21,8 +21,6 @@ const db = getDatabase(app);
 
 
 let table = document.getElementById("mytable");
-table.style.borderWidth = "20px";
-table.style.borderColor = "red";
 
 function insertRow(data) {
     const row = table.insertRow(-1);
